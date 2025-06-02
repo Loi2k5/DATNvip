88533bd9d76d6548c75a10c8cacb12fa3bfc7b8a
anhchien2509 <chienlaps38508@gmail.com>
2025-06-02 19:57:35 +0700

    tao player tam thoi
    Chien

---
+++ Assets/Chien/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    [SerializeField] private float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 inputDirection;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        // Nhận input từ bàn phím
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        inputDirection = new Vector2(moveX, moveY).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = inputDirection * moveSpeed;
+    }
+}

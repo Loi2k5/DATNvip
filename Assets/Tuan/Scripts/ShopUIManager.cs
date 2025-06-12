f70fbca9b44c4c03c7edaf59f62431a9c8ed7218
Minh Tuấn <minhtuan5104@gmail.com>
2025-06-12 22:10:10 +0700

    Làm cửa hàng

---
+++ Assets/Tuan/Scripts/ShopUIManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ShopUIManager : MonoBehaviour
+{
+    public GameObject shopPanel; // Panel Shop
+    public Button shopButton;    // Biểu tượng Shop (Button TMP)
+    public Button exitButton;    // Nút Exit
+
+    void Start()
+    {
+        // Đảm bảo Shop ban đầu ẩn
+        shopPanel.SetActive(false);
+
+        // Gán sự kiện khi bấm vào biểu tượng Shop
+        shopButton.onClick.AddListener(OpenShop);
+
+        // Gán sự kiện khi bấm nút Exit
+        exitButton.onClick.AddListener(CloseShop);
+    }
+
+    void OpenShop()
+    {
+        shopPanel.SetActive(true);  // Hiển thị Shop
+        shopButton.gameObject.SetActive(false);  // Ẩn biểu tượng Shop
+    }
+
+    void CloseShop()
+    {
+        shopPanel.SetActive(false); // Ẩn Shop
+        shopButton.gameObject.SetActive(true);   // Hiển thị lại biểu tượng Shop
+    }
+}

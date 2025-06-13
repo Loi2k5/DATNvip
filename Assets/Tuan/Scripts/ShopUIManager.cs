f427d52f631fbc8d817f0677df3606b222865276
Minh Tuấn <minhtuan5104@gmail.com>
2025-06-13 16:18:38 +0700

    bsung CH

--- Assets/Tuan/Scripts/ShopUIManager.cs
+++ Assets/Tuan/Scripts/ShopUIManager.cs
@@ -7,28 +7,79 @@ public class ShopUIManager : MonoBehaviour
     public GameObject shopPanel; // Panel Shop
     public Button shopButton;    // Biểu tượng Shop (Button TMP)
     public Button exitButton;    // Nút Exit
+    public Button nextButton;    // Nút Next
+    public Button prevButton;    // Nút Previous
+    public GameObject[] pages;   // Danh sách các trang Shop
+    private int currentPage = 0; // Trang hiện tại
 
     void Start()
     {
-        // Đảm bảo Shop ban đầu ẩn
-        shopPanel.SetActive(false);
+        shopPanel.SetActive(false); // Shop bắt đầu ẩn
+        shopButton.gameObject.SetActive(true);
+        nextButton.gameObject.SetActive(false);
+        prevButton.gameObject.SetActive(false);
 
-        // Gán sự kiện khi bấm vào biểu tượng Shop
-        shopButton.onClick.AddListener(OpenShop);
-
-        // Gán sự kiện khi bấm nút Exit
         exitButton.onClick.AddListener(CloseShop);
+        shopButton.onClick.AddListener(OpenShop);
+        nextButton.onClick.AddListener(NextPage);
+        prevButton.onClick.AddListener(PrevPage);
+
+        currentPage = 0;
+        UpdatePage();
     }
 
     void OpenShop()
     {
-        shopPanel.SetActive(true);  // Hiển thị Shop
-        shopButton.gameObject.SetActive(false);  // Ẩn biểu tượng Shop
+        shopPanel.SetActive(true);
+        shopButton.gameObject.SetActive(false);
+        nextButton.gameObject.SetActive(pages.Length > 1); // Hiển thị nếu có nhiều trang
+        prevButton.gameObject.SetActive(false); // Ẩn nút Previous khi mở trang đầu
+        UpdatePage();
     }
 
     void CloseShop()
     {
-        shopPanel.SetActive(false); // Ẩn Shop
-        shopButton.gameObject.SetActive(true);   // Hiển thị lại biểu tượng Shop
+        shopPanel.SetActive(false);
+        shopButton.gameObject.SetActive(true);
+        nextButton.gameObject.SetActive(false);
+        prevButton.gameObject.SetActive(false);
+
+        // Ẩn tất cả các trang khi đóng Shop
+        foreach (GameObject page in pages)
+        {
+            page.SetActive(false);
+        }
+
+        currentPage = 0; // Reset về trang đầu
+    }
+
+    void UpdatePage()
+    {
+        for (int i = 0; i < pages.Length; i++)
+        {
+            pages[i].SetActive(i == currentPage);
+        }
+
+        // Kiểm tra nút Next/Previous
+        nextButton.gameObject.SetActive(currentPage < pages.Length - 1);
+        prevButton.gameObject.SetActive(currentPage > 0);
+    }
+
+    void NextPage()
+    {
+        if (currentPage < pages.Length - 1)
+        {
+            currentPage++;
+            UpdatePage();
+        }
+    }
+
+    void PrevPage()
+    {
+        if (currentPage > 0)
+        {
+            currentPage--;
+            UpdatePage();
+        }
     }
 }

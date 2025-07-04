0a0dbe47468823b09f9d07eef72f66ddc665a0f6
Loi2k5 <169139352+Loi2k5@users.noreply.github.com>
2025-07-04 13:51:04 +0700

    map3

---
+++ Assets/Loi/Scripts/MenuManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    // Gọi khi nhấn nút Play
+    public void OnPlayButton()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1); // Chuyển sang màn tiếp theo
+    }
+
+    // Gọi khi nhấn nút Quit
+    public void OnQuitButton()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit(); // Thoát game khi build
+    }
+    public void OnBackToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu"); // Đảm bảo tên scene là chính xác
+    }
+
+}

e253965939d85b7b648a6cf217162c904bc36562
Longhm2005 <longltps40663@gmail.com>
2025-07-12 18:32:51 +0700

    ket

---
+++ Assets/Long/Scripts/VictoryUI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VictoryUI : MonoBehaviour
+{
+    public void OnMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu"); // Tên scene menu chính của bạn
+    }
+
+    public void OnReplay()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); // Tải lại màn chơi hiện tại
+    }
+
+    public void OnQuit()
+    {
+        Application.Quit(); // Thoát game (chỉ hoạt động khi build game)
+        Debug.Log("Quit game"); // Dùng cho khi chạy trong Editor
+    }
+}

693ca21775f4252dee0e557f1edb2fd0ed9fd863
Longhm2005 <longltps40663@gmail.com>
2025-07-18 01:46:26 +0700

    NPC

---
+++ Assets/Long/Scripts/NPCDialogueTrigger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class NPCDialogueTrigger : MonoBehaviour
+{
+    public GameObject dialoguePanel;                   // Panel chứa đoạn hội thoại
+    public TextMeshProUGUI dialogueText;               // Text TMP để hiển thị lời thoại
+    public string message = "Shin... Cuối cùng chúng ta cũng gặp lại con. Bố mẹ tự hào về con hơn bất cứ điều gì!";
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            dialoguePanel.SetActive(true);
+            dialogueText.text = message;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            dialoguePanel.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Long/Scripts/ShinParentsAppear.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ShinParentsAppear : MonoBehaviour
+{
+    [SerializeField] private GameObject shinParents; // GameObject chứa cả bố mẹ
+
+    private bool appeared = false;
+
+    void Update()
+    {
+        if (!appeared && GameFlags.bossIsDead)
+        {
+            shinParents.SetActive(true); // Hiện NPC
+            appeared = true;
+        }
+    }
+}

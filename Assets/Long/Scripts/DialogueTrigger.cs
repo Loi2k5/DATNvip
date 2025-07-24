94bd9a19cbd77531ebe5d604e23bc91f701dca08
Longhm2005 <longltps40663@gmail.com>
2025-07-24 23:12:21 +0700

    End

--- Assets/Long/Scripts/DialogueTrigger.cs
+++ Assets/Long/Scripts/DialogueTrigger.cs
@@ -2,21 +2,24 @@
 using TMPro;
 using System.Collections;
 
-public class DialogueTyper : MonoBehaviour
+public class NPCDialogueTriggerWithStory : MonoBehaviour
 {
     public GameObject dialoguePanel;
     public TextMeshProUGUI dialogueText;
+    public GameObject storyText;
 
     [TextArea(2, 5)]
     public string message = "Shin!, con trai của chúng ta đã trưởng thành rồi!";
     public float typingSpeed = 0.04f;
 
     private Coroutine typingCoroutine;
+    private bool isTyping = false;
+    private bool storyShown = false;
 
     void Start()
     {
-        if (dialoguePanel != null)
-            dialoguePanel.SetActive(false);
+        if (dialoguePanel != null) dialoguePanel.SetActive(false);
+        if (storyText != null) storyText.SetActive(false);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -44,6 +47,12 @@ public class DialogueTyper : MonoBehaviour
 
         if (dialoguePanel != null)
             dialoguePanel.SetActive(false);
+
+        if (storyText != null)
+            storyText.SetActive(false);
+
+        isTyping = false;
+        storyShown = false;
     }
 
     IEnumerator TypeDialogue()
@@ -51,11 +60,33 @@ public class DialogueTyper : MonoBehaviour
         if (dialogueText == null) yield break;
 
         dialogueText.text = "";
+        isTyping = true;
 
         foreach (char letter in message)
         {
             dialogueText.text += letter;
             yield return new WaitForSeconds(typingSpeed);
+        }
+
+        isTyping = false;
+        ShowStory();
+    }
+
+    void ShowStory()
+    {
+        if (!storyShown && storyText != null)
+        {
+            storyText.SetActive(true);
+            storyShown = true;
+        }
+    }
+
+    void Update()
+    {
+        // Nhấn Space để tắt storyText nếu đang hiện
+        if (storyShown && Input.GetKeyDown(KeyCode.Space))
+        {
+            storyText.SetActive(false);
         }
     }
 }
--- Assets/Long/Scripts/TypingEffect.cs
+++ Assets/Long/Scripts/TypingEffect.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using UnityEngine;
 using TMPro;
-using UnityEngine.UI;
 
 public class TypingEffect : MonoBehaviour
 {
@@ -9,25 +8,42 @@ public class TypingEffect : MonoBehaviour
     [TextArea(5, 20)] public string fullText;
     public float typingSpeed = 0.04f;
 
-    public GameObject storyImage; // ← ảnh nền minh họa
+    public GameObject storyImage; // Ảnh nền minh họa
+
+    private bool isTyping = false;
 
     private void Start()
     {
         targetText.text = "";
-        storyImage.SetActive(true); // ← Cho ảnh hiện ngay từ đầu
+        storyImage.SetActive(true); // Cho ảnh hiện ngay từ đầu
+        StartTyping(); // Gọi typing ngay khi bắt đầu
+    }
+
+    public void StartTyping()
+    {
+        StopAllCoroutines();
+        targetText.text = "";
+        storyImage.SetActive(true);
         StartCoroutine(TypeText());
     }
 
     IEnumerator TypeText()
     {
+        isTyping = true;
         string current = "";
 
         for (int i = 0; i < fullText.Length; i++)
         {
             current += fullText[i];
             targetText.text = current;
-
             yield return new WaitForSeconds(typingSpeed);
         }
+
+        isTyping = false;
+    }
+
+    public bool IsTyping()
+    {
+        return isTyping;
     }
 }
